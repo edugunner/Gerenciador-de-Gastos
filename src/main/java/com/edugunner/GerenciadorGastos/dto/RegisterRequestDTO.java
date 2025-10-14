23c99e1eb58ad5634c1e1739f5a790cee99dea94
edugunner <edu.santana.gunner@gmail.com>
2025-10-13 23:45:48 -0300

    commit

---
+++ src/main/java/com/edugunner/GerenciadorGastos/dto/RegisterRequestDTO.java
@@ -0,0 +1,4 @@
+package com.edugunner.GerenciadorGastos.dto;
+
+public record RegisterRequestDTO (String name, String email, String password) {
+}
---
+++ src/main/java/com/edugunner/GerenciadorGastos/dto/ResponseDTO.java
@@ -0,0 +1,4 @@
+package com.edugunner.GerenciadorGastos.dto;
+
+public record ResponseDTO (String name, String token){
+}
